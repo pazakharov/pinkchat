5b34a278b56d9b2e6eb9dfd3f4ecaa6100b4e1a3
redesc <p.a.zakharov@gmail.com>
2020-11-28 22:16:02 +0800

    Add registration

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=yii-chat-bd',
+    'username' => 'yii-chat',
+    'password' => '123123',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -3,12 +3,13 @@
 namespace app\controllers;
 
 use Yii;
-use yii\filters\AccessControl;
-use yii\web\Controller;
 use yii\web\Response;
-use yii\filters\VerbFilter;
+use yii\web\Controller;
 use app\models\LoginForm;
+use app\models\SignupForm;
 use app\models\ContactForm;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
 
 class SiteController extends Controller
 {
@@ -85,6 +86,29 @@ class SiteController extends Controller
             'model' => $model,
         ]);
     }
+
+    
+    /**
+     * Форма регистрации.
+     *
+     * @return mixed
+     */
+    public function actionSignup()
+    {
+        $model = new SignupForm();
+        if ($model->load(Yii::$app->request->post())) {
+            if ($user = $model->signup()) {
+                if (Yii::$app->getUser()->login($user)) {
+                    return $this->goHome();
+                }
+            }
+        }
+    
+        return $this->render('signup', [
+            'model' => $model,
+        ]);
+    }
+   
 
     /**
      * Logout action.
